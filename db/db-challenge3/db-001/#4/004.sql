5f8abad9e5215ac83df68fb4d6167b4fbf2e60b9
小泉結以奈 <mizugoro1012@gmail.com>
2020-09-24 17:04:35 +0900

    課題3 1 初コミット sql文とキャプチャー添付

---
+++ db/db-challenge3/db-001/#4/004.sql
@@ -0,0 +1,17 @@
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(1,'このチャットの注意事項を添付しました。確認してください。', '注意事項.pdf', 0,'2020/04/21 13:11:55', 1,'2020/04/21 13:11:55', 1);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(1,'確認しました', null, 0,'2020/04/22 16:10:11', 2,'2020/04/22 16:10:11', 2);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(1,'確認しました！', null, 0,'2020/04/22 17:45:29',3 ,'2020/04/22 17:45:29', 3);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(1,'確認しました。', null, 0,'2020/04/22 18:50:11',4 ,'2020/04/22 18:50:11', 4);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(2,'sqlに関する情報を集めるチャットです！よろしくお願いします！', 'sql.png', 0,'2020/04/24 13:41:40', 2 ,'2020/04/24 13:41:40', 2);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(2,'よおｒしくおねがいします！', null, 1,'2020/04/24 13:42:03', 3,'2020/04/24 13:42:42', 3);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(2,'よろしくお願いします！',null , 0,'2020/04/24 13:44:03', 3,'2020/04/24 13:44:03', 3);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(3,'プログラミング言語に関するチャットです！','プログラミング.gif', 0,'2020/04/25 08:06:49', 10,'2020/04/25 08:06:49', 10);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(3,'よろしくお願いします！',null , 0,'2020/04/25 15:42:22', 8,'2020/04/25 15:42:22', 8);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(3,'よろしく！',null, 0,'2020/04/25 19:51:10', 9,'2020/04/25 19:51:10', 9);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(4,'test', null, 0,'2020/04/25 21:15:26', 5,'2020/04/25 21:15:26', 5);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(4,'見れました！', null, 0,'2020/04/26 00:12:43', 7,'2020/04/26 00:12:43', 7);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(5,'test', null, 0,'2020/04/26 10:12:58', 1,'2020/04/26 10:12:58', 1);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(6,'test', null, 0,'2020/04/26 11:21:44', 1,'2020/04/26 11:21:44', 1);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(6,'miremasita', null, 1,'2020/04/26 14:21:32', 3,'2020/04/26 14:22:45', 3);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(7,'test', null, 0,'2020/04/26 16:22:50', 1,'2020/04/26 16:22:50', 1);
+insert into posts (chat_room_id, content, attachment_file_name, is_deleted, posted_at, posted_by, updated_at, updated_by) values(7,'sss', null, 1,'2020/04/26 17:11:23', 10,'2020/04/26 18:09:37', 10);
---
+++ db/db-challenge3/db-001/#5/005.sql
@@ -0,0 +1,9 @@
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('利用規約をアップロードする', 1, 1,'2020/04/21 14:00:00',1, 0,'2020/04/21 11:30:55', 1,'2020/04/21 13:13:20',1);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('利用規約の確認', 5, 1,null,0,0,'2020/04/21 14:02:21',1,'2020/04/22 18:52:20', 4);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('sqlの画像をアップロードする', 2, 2,'2020/04/24 12:30:00',1, 0,'2020/04/23 18:25:24', 2,'2020/04/24 13:43:12',2);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('プログラミングに関する画像アップロードする', 10, 3,'2020/04/25 08:45:00',1,0,'2020/04/24 19:16:34', 10,'2020/04/25 08:08:29',10);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('投稿が見えるかチェック', 6, 4,null,0, 0,'2020/04/24 12:01:15',5,'2020/04/26 01:06:43', 7);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('投稿が見えるかチェック', 2, 5,null,0, 0,'2020/04/26 09:21:58',1,'2020/04/26 09:21:58',1);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('投稿が見えるかチェック', 3, 6,null,0, 0,'2020/04/26 11:22:44',1,'2020/04/26 11:22:44',1);
+insert into tasks (content, person_in_charge, chat_room_id, deadline, is_completed, is_deleted, created_at, created_by, updated_at, updated_by) values('投稿が見えるかチェック', 10, 7,null,0, 0,'2020/04/26 13:23:50',1,'2020/04/26 13:23:50', 1);
+
