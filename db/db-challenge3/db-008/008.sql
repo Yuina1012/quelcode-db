5ba097616435fd4a97b4f041f8e9f933d2a9c091
小泉結以奈 <mizugoro1012@gmail.com>
2020-09-28 19:51:05 +0900

    008sql修正

--- db/db-challenge3/db-008/008.sql
+++ db/db-challenge3/db-008/008.sql
@@ -1,1 +1,8 @@
-SELECT `user_id`, u2.`name`, `chat_room_id`, c.`name`, substring(`joined_at`, 1, 10) as `joined_at` FROM `users_chat_rooms` join `users` as u2 on `user_id` = u2.`id` JOIN `chat_rooms` as c on `chat_room_id` = c.`id` order by chat_room_id;
+SELECT  u2.name, c.name, SUBSTRING(joined_at, 1, 10) AS joined_at
+FROM users_chat_rooms 
+JOIN users AS u2 
+ON user_id = u2.id 
+JOIN chat_rooms AS c 
+ON chat_room_id = c.id
+WHERE u2.is_deleted = 0 AND c.is_deleted = 0
+ORDER BY joined_at ASC;
