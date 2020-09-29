fbf6046d1e0f4865b818e14161e794662ff9612b
小泉結以奈 <mizugoro1012@gmail.com>
2020-09-29 16:10:28 +0900

    010sql実行

--- db/db-challenge3/db-010/0010.sql
+++ db/db-challenge3/db-010/0010.sql
@@ -1,1 +1,14 @@
-SELECT u.name, p.posted_at, c.name FROM posts AS p JOIN users AS u ON p.posted_by = u.id JOIN chat_rooms AS c ON p.chat_room_id = c.id WHERE p.is_deleted=0 AND u.is_deleted=0 AND posted_at IN(SELECT MAX(posted_at) FROM posts GROUP BY chat_room_id HAVING max(posted_at)) ORDER BY chat_room_id;
+SELECT c.name, d.`最新投稿日`, u.name
+FROM (SELECT p.chat_room_id AS `チャットルームID`,MAX(p.posted_at) AS `最新投稿日` 
+      FROM posts AS p 
+      JOIN users AS u 
+      ON p.posted_by = u.id 
+      WHERE u.is_deleted =0 AND p.is_deleted =0 
+      GROUP BY p.chat_room_id) AS d
+INNER JOIN posts p
+ON p.chat_room_id=d.`チャットルームID`AND p.posted_at = d.`最新投稿日`
+JOIN chat_rooms AS c
+ON p.chat_room_id = c.id
+JOIN users AS u
+ON p.posted_by = u.id
+ORDER BY p.chat_room_id;
