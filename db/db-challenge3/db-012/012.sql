5c22f658fb8832f42ec70da7460e324b00286ab2
小泉結以奈 <mizugoro1012@gmail.com>
2020-10-01 16:26:09 +0900

    012 更新者設定

--- db/db-challenge3/db-012/012.sql
+++ db/db-challenge3/db-012/012.sql
@@ -2,7 +2,7 @@ BEGIN;
 UPDATE chat_rooms AS a
 JOIN users_chat_rooms AS b
 ON a.id = b.chat_room_id
-SET a.is_attachable = 0
+SET a.is_attachable = 0, A.updated_by = 1
 WHERE b.chat_room_id not in (SELECT chat_room_id FROM  users_chat_rooms
 WHERE user_id =1 
 GROUP by chat_room_id);
