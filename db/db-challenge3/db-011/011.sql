5add0042acc96e5df1923df818a6e851dff33bba
小泉結以奈 <mizugoro1012@gmail.com>
2020-10-01 16:21:24 +0900

    011SET句に更新者指定

--- db/db-challenge3/db-011/011.sql
+++ db/db-challenge3/db-011/011.sql
@@ -1,7 +1,6 @@
-BEGIN;
+11BEGIN;
 UPDATE tasks
-SET is_completed = 1
+SET is_completed = 1, updated_by = 1
 WHERE created_at BETWEEN '2020-4-26 09:00:00' AND '2020-4-26 11:30:00'
-AND is_deleted = 0
-AND updated_by = 1;
+AND is_deleted = 0;
 COMMIT;
